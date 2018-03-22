7c92a2f711e87de614c524b34899483b3bbb3c56
neoxwf <36872731+neoxwf@users.noreply.github.com>
2018-03-22 09:40:39 +0800

    WooHoo!! Chapter 06 works!

--- Chapter06/HTML/js/z2c-NLC.js
+++ Chapter06/HTML/js/z2c-NLC.js
@@ -14,36 +14,14 @@
  * limitations under the License.
  */
 // displayNLC results
-function checkNLC(_display, _source)
-{
-  var options = {};
-  options.cquery = _source[0].innerText;
-  $.when($.get(_display), $.post('/api/understand/classifyInd', options)).done(function(_page, _nlc_results){
-    console.log("page returned");
-    var _target= $("#modal");
-    _target.append(_page);
-    _target.height($(window).height());
-    _target.show();
-    _data = _nlc_results[0];
-    _classes = JSON.parse(JSON.parse(_data).results).classes;
-    displayNLC($("#industryResult"), _classes);
-    closeNLC=$("#close_NLC");
-    closeNLC.on("click", function(){
-      console.log("closeNLC clicked.")
-      $("#modal").empty();
-    });
-});
-}
 
-function getIndustryClassification(_source, _string)
-{
-
-}
-
-function setModal(_display, cbfn, _modalTarget, _results)
-{
-
-}
+/**
+ * format the classes to display in the provided html element
+ * @param {String} _target - Jquery object pointing to the html target element
+ * @param {Array} _results - the array of results returned by the classifier
+ */
+var industryTable = "#industryResult";
+var nlc_classes; var industryPage="displayNLC.html";
 
 function displayNLC(_target, _results)
 {
@@ -61,4 +39,55 @@ function displayNLC(_target, _results)
     _idx++;
   }
   target.append("</table>");
+}
+
+/**
+ * This is a rewrite of the checkNLC function from the previous chapter to integrated the 
+ * checking process into the custom dialog for this chapter and to save the classification results
+ * for later display, should the user choose that option
+ * @param {String} _target - Jquery object pointing to the html target element
+ * @param {Array} _results - the array of results returned by the classifier
+ */
+function getIndustryClassification(_source, _string)
+{
+  var options = {};
+  options.cquery = _source[0].innerText;
+  $.when( $.post('/api/understand/classifyInd', options)).done(function(_nlc_results){
+    _data = _nlc_results;
+    nlc_classes = JSON.parse(JSON.parse(_data).results).classes;
+    industry = nlc_classes[0].class_name;
+    toggle_mic(_mic, _stop, false)
+    talkToMe(dialog_target, _string.format(getCookieValue("name"), industry));
+  });
+}
+
+/**
+ * This function extracts the code which created the 'modal' window in the previous chapter and
+ * and puts it into a new function. The major change to the logic in this routine is the
+ * addition of the 'cbfn' parameter. cbfn is an abbreviation which I use to indicate that this
+ * is a CallBackFuNction. That is, when setModal is called, it expects a function name to be
+ * passed in as a parameter. That function is called when the modal pattern is in place; in the
+ * logic of this application, the callback function will be the displayNLC results logic.
+ * This approach allows us to use the displayNLC logic without change (lower short and long term maintenance).
+ * @param {String} _display - html page to load
+ * @param {javascript function name} cbfn - the callback function to invoke when complete
+ * @param {String} _modalTarget - Jquery object pointing to the html target element
+ * @param {Array} _results - the array of results returned by the classifier
+ */
+function setModal(_display, cbfn, _modalTarget, _results)
+{
+  $.when($.get(_display)).done(function(_page){
+    var _target= $("#modal");
+    _target.append(_page);
+    _target.height($(window).height());
+    _target.show();
+    closeNLC=$("#close_NLC");
+    closeNLC.on("click", function(){
+      $("#modal").empty();
+      nextStep();
+    });
+    // new to this chapter, invoke the callback function which was passed in to this
+    // routine when it was invoked.
+    cbfn($(_modalTarget), _results);
+  });
 }
--- Chapter06/HTML/js/z2c-speech.js
+++ Chapter06/HTML/js/z2c-speech.js
@@ -16,84 +16,133 @@
 /*
 ** z2c-speech.js
 */
-
+var a_player_target = "#a_player";
+var NLC_Button ; var stt_out; var msg_out;
+var _mic; var _stop;
+var b_token;
+/**
+ * invoke this function when the html page has completed loading
+ */
 function initPage ()
 {
-  var _mic = $('#microphone'); var _stop = $("#stop");
-  var readText = $("#readText"); var displayNLC = $("#classifySpeech");
-  var stt_out = $("#speech");
-  var stream = null;
-  _mic.addClass("mic_enabled");
-  _stop.addClass("mic_disabled");
+  // initialize the b_token value to false. This means that we have not yet retrieved the speech to text token. 
+  b_token = false;
+  // create a cookie to hold the speech to text token
+  document.cookie = "stt-token=";
+  _mic = $('#microphone'); _stop = $("#stop");
+  var readText = $("#readText"); NLC_Button = $("#classifySpeech");
+  // where do we place speech to text output
+  var stt_target = '#speech'; stt_out = $(stt_target);
+  // local variables to hold chat and conversation html targets
+  var chat = $('#chat'); var dialog_target = '#conversation';
+  var stream;
+  // code refactor!
+  // the inline code we used to enable and disable the microphone and stop buttons as been
+  // refactored into a single function call, so that we stop replicating that logic
+  // throughout the code.
+  toggle_mic(_mic, _stop, false)
 
   _mic.on("click", function ()
     {
       var _className = this.className;
       if(this.className == "mic_enabled")
       {
-        _mic.addClass("mic_disabled");
-        _mic.removeClass("mic_enabled");
-        _stop.addClass("mic_enabled");
-        _stop.removeClass("mic_disabled");
-// extract speech to text to separate function
-        talk(stt_out);
+        // invoke the toggle function
+        toggle_mic(_mic, _stop, true)
+        // get whatever is the next step in the dialog. That logic is defined and controlled in the z2c-dialog.js file
+        nextStep();
         }
       });
 
   _stop.on("click",  function()
   {
-    console.log("Stopping speech-to-text service...");
-    if (stream != undefined) {stream.stop(); }
-    _mic.addClass("mic_enabled");
-    _mic.removeClass("mic_disabled");
-    _stop.addClass("mic_disabled");
-    _stop.removeClass("mic_enabled");
-  });
-
-  readText.on("click",  function()
-  {
-    console.log("initiating text-to-speech service...");
-    if (stream != undefined) {stream.stop(); }
-    _mic.addClass("mic_enabled");
-    _mic.removeClass("mic_disabled");
-    _stop.addClass("mic_disabled");
-    _stop.removeClass("mic_enabled");
-
-// extrak the speaking part to a new function.
-    speak($("#chat").val(), a_player, true);
-    $('body').css('cursor', 'wait');
-    $('.readText').css('cursor', 'wait');
-    return true;
-  });
-
-    displayNLC.on("click",  function()
+    if(this.className == "mic_enabled")
     {
-      var nlcPage = "displayNLC.html";
-      checkNLC(nlcPage, stt_out);
-    });
-}
-// create a function to handle speech to text in general
-// pass in the name of the HTML object which will display the received text.
-function talk(_stt_out)
-{
-  $.when($.get('/api/speech-to-text/token')).done(
-    function (token) {
-      stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
-         token: token,
-         outputElement: '#'+_stt_out // CSS selector or DOM Element
-       });
-      stream.on('error', function(err) { console.log(err); });
-    });
+    toggle_mic(_mic, _stop, false)
+    nextStep();
+    }
+  });
 
+  NLC_Button.on("click",  function()
+    {
+      nextStep();
+    });
+    startDialog(dialog_target);
+}
+/**
+ * This is a refactoring of the logic from previous chapters which toggled the state
+ * of the microphone and stop icons. 
+ * @param {jQuery object} _microphone - Jquery object pointing to the microphone html element
+ * @param {jQuery object} _stopbutton - Jquery object pointing to the stop html element
+ * @param {boolean} b_on - boolean flag identifying if this is to enable the stop button (true) or the microphone (false)
+ */
+function toggle_mic(_microphone, _stopbutton, b_on)
+{
+  if(b_on) // microphone button clicked, enable stop button
+  {
+    _microphone.addClass("mic_disabled");
+    _microphone.removeClass("mic_enabled");
+    _stopbutton.addClass("mic_enabled");
+    _stopbutton.removeClass("mic_disabled");
+  } else // stop button clicked, enable microphone button
+  {
+    if (!((typeof(stream) == "undefined") || (stream == null))) {stream.stop(); }
+    _microphone.addClass("mic_enabled");
+    _microphone.removeClass("mic_disabled");
+    _stopbutton.addClass("mic_disabled");
+    _stopbutton.removeClass("mic_enabled");
+  }
+}
+
+/**
+ * create a function to handle speech to text in general
+ * pass in the name of the HTML object which will display the received text. 
+ * This function has been refactored so that the get token function is called only once, rather
+ * than every time the microphone has been activated.
+ * @param {jQuery object} _target - Jquery object pointing to the speech to text output html element
+ */
+function listen(_target)
+{
+  // b_token is a boolean (e.g. only true or false) flag that tells us if we have previously retrieved a token 
+  if (!b_token)
+  {
+    // get the token
+    $.when($.get('/api/speech-to-text/token')).done(
+      function (token) {
+        // save the token returned by the server
+        document.cookie = "stt-token="+token;
+        // set the boolean flag to indicate that we have both retrieved AND saved the token
+        b_token = true
+        // set up the stream from the microphone to Watson
+        stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
+           token: token,
+           outputElement: _target // CSS selector or DOM Element
+         });
+         // if there is an error, display it on the browser console.
+        stream.on('error', function(err) { console.log(err); });
+      });
+  }else
+  {
+      // set up the stream from the microphone to Watson
+      stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
+        // retrieve the previously saved token
+      token: getCookieValue("stt-token"),
+      outputElement: _target // CSS selector or DOM Element
+    });
+    // if there is an error, display it on the browser console.
+   stream.on('error', function(err) { console.log(err); });
+
+  }
 }
 // create a function to handle text to speech in general
 // pass in the text string to be read, the audio player to use and if the audio player should be displayed.
-function speak (_chat, _a_player, b_display)
+function speak (_chat, _a_player_target, b_display)
 {
   var sessionPermissions = JSON.parse(localStorage.getItem('sessionPermissions')) ? 0 : 1;
   var textString = _chat;
   var voice = 'en-US_AllisonVoice';
-  var audio = $("#"+_a_player).get(0);
+  var audioFrame = $(_a_player_target);
+  var audio = audioFrame.get(0);
   var synthesizeURL = '/api/text-to-speech/synthesize' +
     '?voice=' + voice +
     '&text=' + encodeURIComponent(textString) +
@@ -103,12 +152,11 @@ function speak (_chat, _a_player, b_display)
   audio.addEventListener('canplaythrough', onCanplaythrough);
   audio.muted = true;
   audio.play();
-  (b_display) ? audio.show() : audio.hide();
+  (b_display) ? audioFrame.show() : audioFrame.hide();
 }
 
 function onCanplaythrough() {
-  console.log('onCanplaythrough');
-  var audio = $('#a_player').get(0);
+  var audio = $(a_player_target).get(0);
   audio.removeEventListener('canplaythrough', onCanplaythrough);
   try { audio.currentTime = 0; }
   catch(ex) { // ignore. Firefox just freaks out here for no apparent reason.
